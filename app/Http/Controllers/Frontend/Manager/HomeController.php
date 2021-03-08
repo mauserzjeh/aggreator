823f74b97ad590903f03d2dbff594f1dc9e709c9
Soma Rádóczi <mauserzjeh@gmail.com>
2021-03-08 09:55:13 +0100

    Revert "Revert "basic login, register and authentication""
    This reverts commit 1bcc3d56b291c12cf8499b718a67c4bf7a016176.

---
+++ app/Http/Controllers/Frontend/Manager/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Frontend\Manager;
+
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller {
+    
+    public function index() {
+        echo "manager home controller TODO";
+    }
+}
