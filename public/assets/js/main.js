186f0297ec2f6d28ceed36bc99616a418b94303b
Soma Rádóczi <mauserzjeh@gmail.com>
2021-03-14 15:00:47 +0100

    error class

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,15 +1,16 @@
 $(() => {
     $.validator.setDefaults({
+        errorClass: 'is-invalid',
         errorElement: 'div',
         errorPlacement: (error, element) => {
             error.addClass('invalid-feedback form-feedback');
             element.closest('.form-group').append(error);
         },
         highlight: (element, errorClass, validClass) => {
-            $(element).addClass('is-invalid');
+            $(element).addClass(errorClass);
         },
         unhighlight: (element, errorClass, validClass) => {
-            $(element).removeClass('is-invalid');
+            $(element).removeClass(errorClass);
         }
     });
 
