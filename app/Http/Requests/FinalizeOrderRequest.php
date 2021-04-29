03d8b4abd7afa1bfa53f40ee232c8dd938bd2e8d
NMi <nmi986@indamail.hu>
2021-04-29 21:10:50 +0200

    order validation

--- app/Http/Requests/FinalizeOrderRequest.php
+++ app/Http/Requests/FinalizeOrderRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Order;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class FinalizeOrderRequest extends FormRequest {
 
@@ -25,6 +27,10 @@ class FinalizeOrderRequest extends FormRequest {
                 'required',
                 'array',
                 'min:1'
+            ],
+            'delivery_type' => [
+                'required',
+                Rule::in(array_keys(Order::DELIVERY_TYPES))
             ]
         ];
     }
