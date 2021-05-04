2e66c04921261cd9dfe9d7a210b26fc7526f9655
Soma Rádóczi <mauserzjeh@gmail.com>
2021-05-04 23:08:33 +0200

    discounts table migrations

---
+++ database/migrations/2021_05_04_202129_create_discounts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('restaurant_id');
+            $table->float('amount_percent');
+            $table->dateTime('start_timestamp');
+            $table->dateTime('end_timestamp');
+            $table->tinyInteger('active');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('restaurant_id')->references('id')->on('restaurants')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discounts');
+    }
+}
